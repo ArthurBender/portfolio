c9f65e0f98bbc7190d48ef0f025305b906e8d444
Arthur Bender <arthurllbender@gmail.com>
2023-01-10 17:05:27 -0300

    Added initial config and navbar

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <title>Arthur Bender - Portfolio</title>
+    </head>
+    <body>
+        <div class="navbar navbar-dark bg-dark navbar-expand-md">
+            <a class="navbar-brand" href="#">Arthur Bender</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapseContent" aria-controls="navbarCollapseContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapseContent">
+                <ul class="navbar-nav m-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projetos</a>
+                    </li>
+                </ul>
+
+                <form class="form-inline">
+                    <span class="navbar-text mr-3">
+                        Idioma / Language
+                    </span>
+                    <select class="custom-select" name="languageSelect" id="languageSelect">
+                        <option value="portuguese">Português</option>
+                        <option value="english">English</option>
+                    </select>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
